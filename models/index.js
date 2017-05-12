041637ee9d07be2865437ab32ce5b36c0c5cdb1c
daniel mcghee <daniel mcghee>
2017-05-12 08:55:58 -0400

    heroku

--- models/index.js
+++ models/index.js
@@ -6,14 +6,15 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 // var config    = require(__dirname + '/..\config\config.json')[env];
-// var config    = require(__dirname + '\\..\\config\\config.json')[env]; this is the one that worked locally
+var config    = require(__dirname + '\\..\\config\\config.json')[env];
 // var config    = requi  re(__dirname + '..\\config/config.json')[env];
 // var config    = require(__dirname + '/../config/config.json')[env];
-var config = require('../config/config');
+
+// var config = require('../config/config.json')[env];
+// console.log(config);
 
 
-
-var db        = {};
+var db = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
